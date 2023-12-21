572c3f4ded78123f537cbbb27b20cc123df9fc26
Yonghee Kim <19yhk91@gmail.com>
2023-12-21 19:52:41 +0900

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
+
+
+//routes
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 3001;
 
@@ -20,10 +27,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser middleware
 app.use(cookieParser());
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -33,14 +39,14 @@ app.use(errorHandler);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
   app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-);
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+  );
 } else {
+
   app.get('/', (req, res) => {
-    res.send('API is running...');
+    res.send('API is running....');
   });
 }
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- frontend/src/constants.jsx
+++ frontend/src/constants.jsx
@@ -1,5 +1,5 @@
-export const BASE_URL = import.meta.env.VITE_NODE_ENV === 'development' ? 'http://localhost:3001' : ''
-
+// export const BASE_URL = import.meta.env.VITE_NODE_ENV === 'development' ? 'http://localhost:3001' : ''
+export const BASE_URL = ''; // If using proxy
 export const PRODUCTS_URL = '/api/products'
 export const USERS_URL = '/api/users'
 export const ORDERS_URL = '/api/orders'
